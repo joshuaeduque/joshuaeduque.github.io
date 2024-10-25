24267a327a3dc928a3b5d90296fce4281686c660
Josh <joshuaeduque@gmail.com>
2024-10-24 21:38:18 -0500

    Added in transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,46 @@
-<section class="grid content-center p-4 w-screen max-w-full h-screen gap-y-6">
-	<p class="font-librebarcode name-responsive">JOSHUA DUQUE</p>
+<script>
+	import { onMount } from 'svelte';
+	import { expoOut } from 'svelte/easing';
+	import { fly } from 'svelte/transition';
 
-	<div>
-		<p class="font-roboto header-responsive">ABOUT ME</p>
-		<p class="font-roboto-mono text-responsive">
-			My name's Joshua Duque and I'm a computer science student at UTSA. Software became my passion
-			as a teen writing cheats for my favorite online video games. Now, I'd like to make it a
-			career. While most of my work is for personal utility, I'd like to diversify in the future.
-			Thank you for visiting and please enjoy.
+	let mounted = false;
+
+	onMount(() => {
+		mounted = true;
+	});
+</script>
+
+{#if mounted}
+	<section class="grid content-center p-4 w-screen max-w-full h-screen gap-y-6">
+		<p in:fly={{ easing: expoOut, y: -100 }} class="font-librebarcode name-responsive">
+			JOSHUA DUQUE
 		</p>
-	</div>
 
-	<div>
-		<p class="font-roboto header-responsive">PROJECTS</p>
-		<p class="font-roboto-mono text-responsive">Project Name</p>
-		<p class="font-roboto-mono text-responsive">Project Name</p>
-		<p class="font-roboto-mono text-responsive">Project Name</p>
-		<p class="font-roboto-mono text-responsive">Project Name</p>
-	</div>
+		<div in:fly={{ easing: expoOut, x: -100 }}>
+			<p class="font-roboto header-responsive">ABOUT ME</p>
+			<p class="font-roboto-mono text-responsive">
+				My name's Joshua Duque and I'm a computer science student at UTSA. Software became my
+				passion as a teen writing cheats for my favorite online video games. Now, I'd like to make
+				it a career. While most of my work is for personal utility, I'd like to diversify in the
+				future. Thank you for visiting and please enjoy.
+			</p>
+		</div>
 
-	<div class="flex flex-row justify-center">
-		<a href="https://github.com/joshuaeduque" target="_blank">
-			<img class="github-responsive" src="/images/github-mark.svg" alt="GitHub logo" />
-		</a>
-	</div>
-</section>
+		<div in:fly={{ easing: expoOut, x: 100 }}>
+			<p class="font-roboto header-responsive">PROJECTS</p>
+			<p class="font-roboto-mono text-responsive">Project Name</p>
+			<p class="font-roboto-mono text-responsive">Project Name</p>
+			<p class="font-roboto-mono text-responsive">Project Name</p>
+			<p class="font-roboto-mono text-responsive">Project Name</p>
+		</div>
+
+		<div in:fly={{ easing: expoOut, y: 100 }} class="flex flex-row justify-center">
+			<a href="https://github.com/joshuaeduque" target="_blank">
+				<img class="github-responsive" src="/images/github-mark.svg" alt="GitHub logo" />
+			</a>
+		</div>
+	</section>
+{/if}
 
 <style lang="postcss">
 	.name-responsive {
