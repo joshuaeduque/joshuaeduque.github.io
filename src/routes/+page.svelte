7a3690e166acade511a0bdd8726bdf3932c1238f
Josh <joshuaeduque@gmail.com>
2024-10-24 21:04:42 -0500

    Added more stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<section class="grid p-4 w-screen max-w-full h-screen content-evenly">
-	<p class="font-librebarcode text-6xl">JOSHUA DUQUE</p>
+<section class="grid content-center p-4 w-screen max-w-full h-screen gap-y-6">
+	<p class="font-librebarcode name-responsive">JOSHUA DUQUE</p>
 
 	<div>
-		<p class="font-roboto text-2xl">ABOUT ME</p>
-		<p class="font-roboto-mono text-lg">
+		<p class="font-roboto header-responsive">ABOUT ME</p>
+		<p class="font-roboto-mono text-responsive">
 			My name's Joshua Duque and I'm a computer science student at UTSA. Software became my passion
 			as a teen writing cheats for my favorite online video games. Now, I'd like to make it a
 			career. While most of my work is for personal utility, I'd like to diversify in the future.
@@ -12,16 +12,34 @@
 	</div>
 
 	<div>
-		<p class="font-roboto text-2xl">PROJECTS</p>
-		<p class="font-roboto-mono text-lg">Project Name</p>
-		<p class="font-roboto-mono text-lg">Project Name</p>
-		<p class="font-roboto-mono text-lg">Project Name</p>
-		<p class="font-roboto-mono text-lg">Project Name</p>
+		<p class="font-roboto header-responsive">PROJECTS</p>
+		<p class="font-roboto-mono text-responsive">Project Name</p>
+		<p class="font-roboto-mono text-responsive">Project Name</p>
+		<p class="font-roboto-mono text-responsive">Project Name</p>
+		<p class="font-roboto-mono text-responsive">Project Name</p>
 	</div>
 
 	<div class="flex flex-row justify-center">
-		<a href="https://github.com/joshuaeduque">
-			<img class="w-8" src="/images/github-mark.svg" alt="GitHub logo" />
+		<a href="https://github.com/joshuaeduque" target="_blank">
+			<img class="github-responsive" src="/images/github-mark.svg" alt="GitHub logo" />
 		</a>
 	</div>
 </section>
+
+<style lang="postcss">
+	.name-responsive {
+		@apply text-6xl sm:text-7xl xl:text-8xl 3xl:text-9xl;
+	}
+
+	.header-responsive {
+		@apply text-2xl sm:text-3xl xl:text-4xl 3xl:text-5xl;
+	}
+
+	.text-responsive {
+		@apply text-lg sm:text-xl xl:text-2xl 3xl:text-3xl;
+	}
+
+	.github-responsive {
+		@apply w-9 sm:w-11 xl:w-14 3xl:w-16;
+	}
+</style>
