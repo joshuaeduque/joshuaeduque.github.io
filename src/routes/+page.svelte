6e31688fd5d15df6f19f26c02e530a659b99a2a5
Josh <joshuaeduque@gmail.com>
2024-10-24 20:22:51 -0500

    Added page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="grid p-4 w-screen max-w-full h-screen content-evenly">
+	<p class="font-librebarcode text-6xl">JOSHUA DUQUE</p>
+
+	<div>
+		<p class="font-roboto text-2xl">ABOUT ME</p>
+		<p class="font-roboto-mono text-lg">
+			My name's Joshua Duque and I'm a computer science student at UTSA. Software became my passion
+			as a teen writing cheats for my favorite online video games. Now, I'd like to make it a
+			career. While most of my work is for personal utility, I'd like to diversify in the future.
+			Thank you for visiting and please enjoy.
+		</p>
+	</div>
+
+	<div>
+		<p class="font-roboto text-2xl">PROJECTS</p>
+		<p class="font-roboto-mono text-lg">Project Name</p>
+		<p class="font-roboto-mono text-lg">Project Name</p>
+		<p class="font-roboto-mono text-lg">Project Name</p>
+		<p class="font-roboto-mono text-lg">Project Name</p>
+	</div>
+
+	<div class="flex flex-row justify-center">
+		<a href="https://github.com/joshuaeduque">
+			<img class="w-8" src="/images/github-mark.svg" alt="GitHub logo" />
+		</a>
+	</div>
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      'librebarcode': 'LibreBarcode128Text',
+      'roboto': 'Roboto',
+      'roboto-light': 'Roboto-Light',
+      'roboto-thin': 'Roboto-Thin',
+      'roboto-mono': 'RobotoMono'
+    },
     extend: {},
   },
   plugins: [],
