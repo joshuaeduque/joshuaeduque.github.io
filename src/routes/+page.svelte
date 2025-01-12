6117679fa6d9df0e98f87dd85d7c48cb56d2350f
Josh <joshuaeduque@gmail.com>
2025-01-12 00:48:22 -0600

    Added projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,23 +13,35 @@
 				</p>
 				<div class="flex gap-4">
 					<a href="https://github.com/joshuaeduque" target="_blank">
-						<img class="w-12 contrast-200" src={githubIcon} alt="GitHub icon" />
+						<img class="w-10 contrast-200 transition hover:scale-105" src={githubIcon} alt="GitHub icon" />
 					</a>
 					<a href="mailto:joshuaeduque@gmail.com">
-						<img class="w-12 contrast-200 transition hover:w-14" src={emailIcon} alt="Email icon" />
+						<img class="w-10 contrast-200 transition hover:scale-105" src={emailIcon} alt="Email icon" />
 					</a>
 				</div>
 			</div>
 		</div>
-		<div class="h-fit flex flex-col justify-center items-center gap-4">
+		<div class="h-fit flex flex-col gap-4">
+			<p class="font-notosansmono-condensed font-bold">projects</p>
 			<div>
-				<p>project</p>
+				<p class="font-notosansmono-condensed underline">
+					<a href="https://github.com/joshuaeduque/ps3mapi" target="_blank">PS3MAPI</a>
+				</p>
+				<p class="font-notosansmono-condensed">Remote communications library for Sony's PlayStation 3 written in C#</p>
 			</div>
+
 			<div>
-				<p>project</p>
+				<p class="font-notosansmono-condensed underline">
+					<a href="https://devpost.com/software/futurefocus" target="_blank">FutureFocus</a>
+				</p>
+				<p class="font-notosansmono-condensed">Groq AI powered career planner awarded best use of Terraform at RowdyHacks X</p>
 			</div>
+
 			<div>
-				<p>project</p>
+				<p class="font-notosansmono-condensed underline">
+					<a href="https://github.com/joshuaeduque/dragonscroll" target="_blank">DragonScroll</a>
+				</p>
+				<p class="font-notosansmono-condensed">Offline personal book logger for Android using Room DB and SharedPreferences libraries</p>
 			</div>
 		</div>
 	</div>
