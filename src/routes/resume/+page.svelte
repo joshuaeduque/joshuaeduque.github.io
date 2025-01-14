c656cb788a977c12f9c75309d1f5ae77f9cd71d6
Josh <joshuaeduque@gmail.com>
2025-01-14 01:05:21 -0600

    Added resume download

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,6 +3,7 @@
 	<p class="font-notosansmono-condensed">
 		You may have to scroll / zoom depending on your screen size
 	</p>
+	<button class="font-notosansmono-condensed underline hvr-sweep-to-right" on:click={downloadResume}>download</button>
 	<canvas id="pdf-canvas"></canvas>
 </div>
 
@@ -11,47 +12,74 @@
 <!-- When running locally, it'll default to the package in node_modules -->
 
 <script>
-	import * as pdfjs from 'pdfjs-dist/legacy/build/pdf.mjs';
+	// @ts-nocheck
+
+	import { onMount } from 'svelte';
 
 	import resume from '$lib/files/resume.pdf';
 
+	import * as pdfjs from 'pdfjs-dist/legacy/build/pdf.mjs';
 	import worker from 'pdfjs-dist/legacy/build/pdf.worker.mjs?url';
 
-	pdfjs.GlobalWorkerOptions.workerSrc = worker;
+	let pdfData;
 
-	const loadTask = pdfjs.getDocument(resume);
-	loadTask.promise
-		.then((pdf) => {
-			pdf.getPage(1).then((page) => {
-				let viewport = page.getViewport({ scale: 1.0 });
-				let outputScale = window.devicePixelRatio || 1;
+	function downloadResume() {
+		if (!pdfData) return;
 
-				let canvas = document.getElementById('pdf-canvas');
-				// @ts-ignore
-				let context = canvas.getContext('2d');
+		const blobUrl = URL.createObjectURL(new Blob([pdfData], { type: 'application/pdf' }));
+		const atag = document.createElement('a');
+		if (!atag.click) {
+			console.error('a.click() not supported');
+			return;
+		}
 
-				// @ts-ignore
-				canvas.width = Math.floor(viewport.width * outputScale);
-				// @ts-ignore
-				canvas.height = Math.floor(viewport.height * outputScale);
-				// @ts-ignore
-				canvas.style.width = Math.floor(viewport.width) + 'px';
-				// @ts-ignore
-				canvas.style.height = Math.floor(viewport.height) + 'px';
+		atag.href = blobUrl;
+		atag.target = '_parent';
 
-				var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+		if ('download' in atag) {
+			atag.download = 'JEDUQUE.pdf';
+		}
 
-				var renderContext = {
-					canvasContext: context,
-					transform: transform,
-					viewport: viewport
-				};
+		(document.body || document.documentElement).append(atag);
 
-				// @ts-ignore
-				page.render(renderContext);
+		atag.click();
+		atag.remove();
+	}
+
+	onMount(() => {
+		pdfjs.GlobalWorkerOptions.workerSrc = worker;
+
+		const loadTask = pdfjs.getDocument(resume);
+		loadTask.promise
+			.then((pdf) => {
+				pdf.getData().then((data) => {
+					pdfData = data;
+				});
+				pdf.getPage(1).then((page) => {
+					let viewport = page.getViewport({ scale: 1.0 });
+					let outputScale = window.devicePixelRatio || 1;
+
+					let canvas = document.getElementById('pdf-canvas');
+					let context = canvas.getContext('2d');
+
+					canvas.width = Math.floor(viewport.width * outputScale);
+					canvas.height = Math.floor(viewport.height * outputScale);
+					canvas.style.width = Math.floor(viewport.width) + 'px';
+					canvas.style.height = Math.floor(viewport.height) + 'px';
+
+					var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+
+					var renderContext = {
+						canvasContext: context,
+						transform: transform,
+						viewport: viewport
+					};
+
+					page.render(renderContext);
+				});
+			})
+			.catch((reason) => {
+				console.error(reason);
 			});
-		})
-		.catch((reason) => {
-			console.error(reason);
-		});
+	});
 </script>
