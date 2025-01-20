35f085950d1bf652491738cc40251a7a640eb286
Josh <joshuaeduque@gmail.com>
2025-01-19 19:30:02 -0600

    Moved links and about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,29 +22,12 @@
 							<a href="/resume">resume</a>
 						</p>
 						<p class="font-notosansmono-condensed underline w-fit hvr-sweep-to-right">
-							<a href="#about-me">about me</a>
+							<a href="https://github.com/joshuaeduque">github</a>
+						</p>
+						<p class="font-notosansmono-condensed underline w-fit hvr-sweep-to-right">
+							<a href="/about">about me</a>
 						</p>
 					</div>
-				</div>
-
-				<div
-					in:fly={{ y: 100, easing: expoOut, duration: flyDuration, opacity: 0.001 }}
-					class="flex gap-4">
-					<a href="https://github.com/joshuaeduque" target="_blank">
-						<!-- Keep title and alt the same for accessibility -->
-						<img
-							class="w-10 brightness-0 transition hover:scale-110"
-							src={githubIcon}
-							title="GitHub link"
-							alt="GitHub link" />
-					</a>
-					<a href="mailto:joshuaeduque@gmail.com">
-						<img
-							class="w-10 transition hover:scale-110"
-							src={emailIcon}
-							title="Email link"
-							alt="Email link" />
-					</a>
 				</div>
 			</div>
 		</div>
@@ -160,42 +143,6 @@
 				</div>
 			</div>
 		</div>
-	</div>
-
-	<div id="about-me" class="p-4">
-		<p class="font-notosansmono-condensed font-bold">about me</p>
-		<p class="font-notosansmono-condensed">
-			My name is Joshua Duque, and I’m a computer science student at the University of Texas at San
-			Antonio. I grew up in a small town with a population of about 3,000, spending my childhood
-			with my mother and older brother. In our living room sat a CRT monitor connected to a desktop
-			running Windows ME, where my mom played solitaire and my brother watched Flash Player videos.
-			I can’t pinpoint the exact moment it happened, but the hum of the machine and the glow of the
-			screen sparked a fascination with computers that’s stuck with me ever since.
-		</p>
-		<br />
-		<p class="font-notosansmono-condensed">
-			As a teenager, I started learning C++ and basic x86/PowerPC assembly to create cheats for my
-			favorite online games. I loved figuring out how each game worked and sharing in-game resources
-			with my friends after a breakthrough. Later, I joined my high school’s robotics team as the
-			only programmer, which introduced me to hardware components and embedded development—something
-			I still enjoy working with today.
-		</p>
-		<br />
-		<p class="font-notosansmono-condensed">
-			After graduating high school, I was uncertain about pursuing higher education right away.
-			Instead, I spent time with family and worked locally during the COVID pandemic. However,
-			someone close to me encouraged me to aim beyond the boundaries of my small town. With their
-			support, I completed an associate degree at my local community college and decided to pursue a
-			bachelor’s degree in computer science, turning my long-standing passion for technology into a
-			career path.
-		</p>
-		<br />
-		<p class="font-notosansmono-condensed">
-			My biggest hope is to inspire others from similar backgrounds to step out of their comfort
-			zones and discover what’s truly possible for themselves. One day, I aspire to give back to my
-			community in a meaningful way that genuinely impacts those who need it most. Thank you for
-			taking the time to read, and I hope you have an amazing day :&#41
-		</p>
 	</div>
 {/if}
 
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<div class="p-4">
+    <p class="font-notosansmono-condensed font-bold">about me</p>
+		<p class="font-notosansmono-condensed">
+			My name is Joshua Duque, and I’m a computer science student at the University of Texas at San
+			Antonio. I grew up in a small town with a population of about 3,000, spending my childhood
+			with my mother and older brother. In our living room sat a CRT monitor connected to a desktop
+			running Windows ME, where my mom played solitaire and my brother watched Flash Player videos.
+			I can’t pinpoint the exact moment it happened, but the hum of the machine and the glow of the
+			screen sparked a fascination with computers that’s stuck with me ever since.
+		</p>
+		<br />
+		<p class="font-notosansmono-condensed">
+			As a teenager, I started learning C++ and basic x86/PowerPC assembly to create cheats for my
+			favorite online games. I loved figuring out how each game worked and sharing in-game resources
+			with my friends after a breakthrough. Later, I joined my high school’s robotics team as the
+			only programmer, which introduced me to hardware components and embedded development—something
+			I still enjoy working with today.
+		</p>
+		<br />
+		<p class="font-notosansmono-condensed">
+			After graduating high school, I was uncertain about pursuing higher education right away.
+			Instead, I spent time with family and worked locally during the COVID pandemic. However,
+			someone close to me encouraged me to aim beyond the boundaries of my small town. With their
+			support, I completed an associate degree at my local community college and decided to pursue a
+			bachelor’s degree in computer science, turning my long-standing passion for technology into a
+			career path.
+		</p>
+		<br />
+		<p class="font-notosansmono-condensed">
+			My biggest hope is to inspire others from similar backgrounds to step out of their comfort
+			zones and discover what’s truly possible for themselves. One day, I aspire to give back to my
+			community in a meaningful way that genuinely impacts those who need it most. Thank you for
+			taking the time to read, and I hope you have an amazing day :&#41
+		</p>
+</div>
